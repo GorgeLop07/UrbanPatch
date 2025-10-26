5b260b6743ed118b872666cdb5a2aa5a9a44db36
GorgeLop07 <jorgelolpez07@gmail.com>
2025-10-26 11:34:01 -0600

    Manejo de ver 1.4: Subir Gemini_develop

---
+++ Gemini_Request/GenAI.py
@@ -0,0 +1,60 @@
+from google import genai
+from google.genai import types
+from pdf import pdf_builder
+import json 
+from flask import Flask
+import requests
+
+#server stuff
+rute = "http://10.22.229.101:8001/api/reportes/top_10_colonias"
+
+# GET request
+response_get = requests.get(rute)
+response_json = response_get.json()
+
+
+API_KEY = "AIzaSyBFqFPO6h7F88h4J_cFA6_Wpgs_DBqmyjY"
+client = genai.Client(api_key=API_KEY)
+responses = []
+
+#JSON READ:
+data = response_json
+No_colonias = 10  #total colonias
+
+nombres = []
+
+for i in range(No_colonias):
+    faltas_criticas = 0
+    faltas_altas = 0
+    faltas_bajas = 0
+
+    nombre_col = data["top_10_colonias"][i]["nombre_colonia"]
+    nombres.append(str(nombre_col))
+    total_reportes = data["top_10_colonias"][i]["total_fallas"]
+    costo_reparacion = total_reportes*450
+
+
+
+    responses.append(client.models.generate_content(
+    model = "gemini-2.5-flash-lite",
+
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_budget=0), # Disables thin]king
+        #settings:
+        #temperature=0.2
+        system_instruction=
+        ("Eres un trabajador bancario y necesitas realizar un reporte para una colonia con problemas de infraestructura "
+         "la respuesta tiene una estructura definida ejemplificada despues las variables usadas son: Nombre colonia, cantidad de fallas, costo de arreglar fallas"
+         "en la siguiente parte usa la reparación completa proveida")
+    ),
+    contents= f'''has un pequeño parrafo sobre los problemas de infraestructura relacionados con pavimentación
+    Aqui hay un ejemplo de como estructurarlo:
+
+    La colonia Benito Juarez a recibido reporte de 10 fallas
+    El costo total aproximado para arreglar todas las fallas es de 3200 pesos
+
+    VARIABLES: nombre colonia = {nombre_col}, total reportes = {total_reportes},  costo de reparación {costo_reparacion}'''
+    ))
+
+pdf_builder(No_colonias, responses, nombres)
+#(responses[i]).text , asi accede a la respuesta por colonia.
---
+++ Gemini_Request/pdf.py
@@ -0,0 +1,123 @@
+from fpdf import FPDF
+from fpdf import Template
+#HEADER FOOTER:
+class FPDF(FPDF):
+
+    def header (self):
+        self.set_font("arial", "B", 14)
+        self.image("Banorte-logo.jpg", h=20)
+        self.set_xy(10,30)
+
+    def footer (self):
+        self.set_y(-20)
+        pageNum = self.page_no()
+        self.cell(0,10,str(pageNum), "Top" ,align="R")
+        #self.rect()
+
+def pdf_builder(No_colonias: int, responses : list, nombres):
+
+    #PDF SETUP
+    pdf = FPDF("p", "mm", "A4") #orientacion, Unidad de medida, dimensiones puede ser en format=(x,y)
+
+    W = 210 #legal: 215.9 A4: 210
+    H = 297 #legal: 355.6 A4: 297
+    border = True
+
+    pdf.set_font("arial", "", 14)
+    pdf.set_margins(10, 10, 10)
+    pdf.add_page()
+
+
+    #page 1
+    pdf.set_font("arial", "B", 16)
+    pdf.set_xy(10,40)
+    pdf.cell(0,10,"Reporte automatizado de infraestructura en colonias de nuevo leon", border ,align="C")
+
+    pdf.set_font("arial", "", 12)
+    pdf.set_xy(15,70)
+    pdf.multi_cell(0,7,"El deterioro de la infraestructura vial es uno de los principales problemas urbanos que afecta la movilidad, la seguridad y la calidad de vida de los habitantes de Monterrey." 
+                   "En diversas colonias del municipio, la presencia de baches ha generado afectaciones tanto económicas como estructurales, incrementando los costos de mantenimiento vehicular  y "
+                   "reduciendo la eficiencia del tránsito. Este reporte presenta un análisis de las colonias con mayores incidencias de baches, identificando las zonas más afectadas y evaluando "
+                   "el impacto que este problema tiene sobre la infraestructura y los servicios públicos.", border)
+
+    pdf.set_font("arial", "B", 14)
+    pdf.set_xy(10,120)
+    pdf.cell(0,10,"Index", border)
+
+    Index_Y = 135
+    pdf.set_font("arial", "", 12)
+    pdf.set_xy(15, Index_Y)
+    Index_Y_step = (H-20-Index_Y)/No_colonias
+
+    for i in range(No_colonias):
+        pdf.set_xy(15, Index_Y+Index_Y_step*i)
+        pdf.cell(0, 10, nombres[i])
+        pdf.set_xy(W*0.75, Index_Y+Index_Y_step*i)
+        pdf.cell(0, 10, f"pg. {i+2}")
+
+    #page n
+    for i in range (No_colonias):
+        pdf.add_page()
+
+        pdf.set_font("arial", "B", 14)
+        pdf.cell(0,10, nombres[i], border)
+        pdf.set_font("arial", "", 12)
+        pdf.set_xy(15,50)
+        #pdf.cell(0,35,"Neighbourhood one text", border)
+        pdf.multi_cell(0,7,responses[i].text, border)
+        pdf.set_xy(15,100)
+        img_sz = 100
+        pdf.image("spong.png", w=img_sz, x= (W-img_sz)/2 )
+
+    
+    
+
+    pdf.output("REPORTE.pdf")
+
+
+#syntasis/doc:
+#CELL
+#cell(width, height, "text", border type, ln, align="l")
+#width = 0 takes all the line from margin to margin
+#border type: 1 = alrededor, "l", "r", "T", "B"
+#ln 1 skips line after cell, ln 0 continues same line, ln 2 puts cursor under cell
+#align = l, l = left, C = center, R = right
+#fill = True, fils with colors, False default
+#link = "link" text becomes link
+
+#SET, sets the cursor at position x,y
+#pdf.set_xy(x,y)
+
+#Multi_cell, cell but multi line and text adjusts to width 
+#pdf.multi_cell(w,h, txt= text, border= 1)
+#h = height of a cell not container
+#same ¿all? parameters as normal cell
+
+#HEADER, define a class
+
+#SHAPES
+#pdf.elipse(x,y,d1,d2,style="FD") D=draw border, F= fill, FD both
+#pdf.rect(x,y,l1,l2,style"DF")
+#pdf.line(x,y,x1,y1)
+#pdf.dashed_line(x1,y1,x2,y2, dash_lenght = n, space_lenght = n)
+
+#COLOR:
+#pdf.set_text_color(R,G,B)
+#pdf.set_draw_color(R,G,B) for borders not fills
+#pdf.set_fill_color(R,G,B) for shapes and cell colors
+
+#IMAGES
+#pdf.image("filename", w=width, h= height, x = xpos, y = ypos ))
+#filename =  if file in folder or direction
+#width and height, if only one defined second is adjusted automatically
+#x pos and y pos
+#can be added to header or foot
+
+#class FPDF(FPDF):
+# def header (self):
+#   self.set_font()
+#   self.cell()
+#   self.set_xy(x,y)
+
+# def footer (self):
+#   pass
